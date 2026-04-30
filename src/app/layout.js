66ddc72fec1691aa7db93cd0618c591d09538644
ovijet halder <ovijethalder143@gmail.com>
2026-04-30 14:03:47 +0600

    navber banner footer added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/component/Navbar";
+import Footer from "@/component/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,8 +23,13 @@ export default function RootLayout({ children }) {
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      data-theme="light"
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navbar />
+        {children}
+        <Footer/>
+      </body>
     </html>
   );
 }
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,78 @@
+"use client";
+import NavLink from "@/share/NavLink";
+// import { authClient } from "@/lib/auth-client";
+import { Avatar, Button } from "@heroui/react";
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  //   const userData = authClient.useSession();
+  //   const user = userData.data?.user;
+
+  //   const handleSignOut = async () => {
+  //     await authClient.signOut();
+  //   }
+
+  return (
+    <div className="border-b px-2">
+      <nav className=" flex justify-between items-center  py-3 max-w-7xl mx-auto w-full">
+        <div className="flex  items-center">
+          <Image
+            src={"/sunLogo.png"}
+            alt="logo"
+            loading="eager"
+            width={60}
+            height={60}
+            className="object-cover h-auto w-auto"
+          />
+          <h3 className="font-bold text-3xl">
+            S<span className="text-orange-600">un</span>Cart.
+          </h3>
+        </div>
+
+        <ul className="flex items-center gap-5 text-sm">
+          <li>
+            <NavLink href={"/"}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink href={"/all-product"}>All Products</NavLink>
+          </li>
+
+          <li>
+            <NavLink href={"/profile"}>Profile</NavLink>
+          </li>
+        </ul>
+
+        <div className="flex gap-4">
+          {
+            <ul className="flex items-center  text-sm gap-5">
+              <li>
+                <NavLink href={"/signup"}>SignUp</NavLink>
+              </li>
+              <li>
+                <NavLink href={"/signin"}>SignIn</NavLink>
+              </li>
+            </ul>
+          }
+
+          {/* {user && (
+            <div className="flex gap-3">
+              <Avatar size="sm">
+                <Avatar.Image
+                  alt="John Doe"
+                  src={user?.image}
+                  referrerPolicy="no-referrer"
+                />
+                <Avatar.Fallback>{user?.name.charAt(0)}</Avatar.Fallback>
+              </Avatar>
+
+              <Button onClick={handleSignOut} size="sm" variant="danger">SignOut</Button>
+            </div>
+          )} */}
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
