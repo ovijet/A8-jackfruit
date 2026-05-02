01315b0591a704b2c8848546ed04125167b8c9c5
ovijet halder <ovijethalder143@gmail.com>
2026-05-02 23:28:36 +0600

    add

--- src/lib/auth.client.js
+++ src/lib/auth.client.js
@@ -1,5 +1,5 @@
-import { createAuthClient } from "better-auth/react"
+import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://a8-jackfruit.vercel.app"
-})
+  /** The base URL of the server (optional if you're using the same domain) */
+  baseURL: process.env.BETTER_AUTH_URL,
+});
